34f165737a4e0dadc729cc3309c46f6662e41813
Guilherme <guilumagaro@gmail.com>
2026-07-21 09:51:43 -0300

    fix: import AuditModule in ReservasModule

--- backend/src/modules/frota/reservas/reservas.module.ts
+++ backend/src/modules/frota/reservas/reservas.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { ReservasController } from "./reservas.controller";
 import { ReservasService } from "./reservas.service";
+import { AuditModule } from "../../audit/audit.module";
 
 @Module({
+  imports: [AuditModule],
   controllers: [ReservasController],
   providers: [ReservasService],
 })
